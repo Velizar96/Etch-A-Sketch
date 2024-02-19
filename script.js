f3fc6da9e4e7a8077aec525abb53110b3c34b1df
Velizar96 <velizar.vesovicc@gmail.com>
2024-02-19 17:42:19 +0100

    I rearranged the functions and cleaned up the code

--- script.js
+++ script.js
@@ -1,22 +1,59 @@
+"use strict";
+
+////SELECTORS////
 const container = document.querySelector(".container");
-container.style.flexWrap = "wrap";
-container.style.alignContent = "flex-start";
+const button = document.querySelector(".btn");
+const randomBtn = document.querySelector(".random");
+const eraseBtn = document.querySelector(".erase");
+////FUNCTIONS////
 
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement("div");
-  div.className = "divs";
+function creatingDivs() {
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.className = "divs";
+    container.style.flexWrap = "wrap";
+    container.style.alignContent = "flex-start";
 
-  container.appendChild(div);
+    container.appendChild(div);
+  }
+}
+creatingDivs();
+// ----------------------------------------------
+function draw() {
+  const divs = document.querySelectorAll(".divs");
+  divs.forEach((div) => {
+    div.style.boxSizing = "border-box";
+    div.addEventListener("mouseover", function (e) {
+      e.target.style.backgroundColor = "black";
+    });
+  });
+}
+draw();
+// ----------------------------------------------
+function randomColorGenerator() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`;
+}
+// ----------------------------------------------
+function randomColor() {
+  const divs = document.querySelectorAll(".divs");
+  divs.forEach((div) => {
+    div.addEventListener("mouseover", function () {
+      div.style.backgroundColor = randomColorGenerator();
+    });
+  });
 }
 
-const button = document.querySelector(".btn");
+//addEventListeners//
+
 button.addEventListener("click", function (e) {
   e = parseInt(prompt("Number of squares per side?"));
-  // myDivs.forEach((div) => {
   while (container.hasChildNodes()) {
     container.removeChild(container.firstChild);
   }
-  // });
+
   for (let i = 0; i < e * e; i++) {
     const div = document.createElement("div");
     div.className = "divs";
@@ -28,37 +65,8 @@ button.addEventListener("click", function (e) {
   }
   draw();
 });
+// ----------------------------------------------
 
-////FUNCTIONS////
-//logic for drawing
-function draw() {
-  const divs = document.querySelectorAll(".divs");
-  divs.forEach((div) => {
-    div.style.boxSizing = "border-box";
-    div.addEventListener("mouseover", function (e) {
-      e.target.style.backgroundColor = "black";
-    });
-  });
-}
-draw();
-
-function randomColorGenerator() {
-  const r = Math.floor(Math.random() * 256);
-  const g = Math.floor(Math.random() * 256);
-  const b = Math.floor(Math.random() * 256);
-  return `rgb(${r},${g},${b})`;
-}
-
-function randomColor() {
-  const divs = document.querySelectorAll(".divs");
-  divs.forEach((div) => {
-    div.addEventListener("mouseover", function () {
-      div.style.backgroundColor = randomColorGenerator();
-    });
-  });
-}
-
-//BUTTONS LOGIC//
 const blackBtn = document.querySelector(".black");
 blackBtn.addEventListener("click", function () {
   const divs = document.querySelectorAll(".divs");
@@ -68,11 +76,11 @@ blackBtn.addEventListener("click", function () {
     });
   });
 });
+// ----------------------------------------------
 
-const randomBtn = document.querySelector(".random");
 randomBtn.addEventListener("click", randomColor);
+// ----------------------------------------------
 
-const eraseBtn = document.querySelector(".erase");
 eraseBtn.addEventListener("click", function () {
   const divs = document.querySelectorAll(".divs");
   divs.forEach((div) => {
